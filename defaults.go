12dc0569f758aff524990b35dedfd8a6278a6f1f
Oula Kuuva <oula.kuuva@iki.fi>
2019-07-08 20:47:17 +0300

    Add example defaults and config file

---
+++ defaults.go
@@ -0,0 +1,14 @@
+// Copyright Oula Kuuva 2019
+package main
+
+var players2IDs = map[string]string{
+	"SteamNick1": "13123discordIDhere123123",
+	"SteamNick2": "23123discordIDhere123123",
+}
+
+var games2Webhooks = map[string]string{
+	"RegularGaem":     "https://discordapp.com/webhook0",
+	"SupaAwesomeGaem": "https://discordapp.com/webhook1",
+}
+
+var debugWebhook = "https://when-all-goes-bonkers"
