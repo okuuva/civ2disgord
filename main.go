1ae0b0c5bb61a03f751ba17b092f32f658aa4bc7
Oula Kuuva <oula.kuuva@iki.fi>
2019-07-12 11:18:01 +0300

    Send messages and check responses in main

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/jinzhu/copier"
 	"github.com/okuuva/civ2disgord/civ2disgord"
@@ -42,5 +44,34 @@ func main() {
 	if len(cmdline.messages) == 0 {
 		logger.checkFatal(errors.New("no message given"), "No message given", 3)
 	}
+	var responses []*http.Response
+	var errs []error
+	for _, message := range cmdline.messages {
+		civMessage, err := civ2disgord.ParseMessage(strings.NewReader(message))
+		logger.checkFatal(err, "Failed to parse message", 4)
+		discordMessage, err := civMessage.NewDefaultDiscordMessage(&config, false)
+		logger.checkFatal(err, "Failed to construct DiscordMessage", 5)
+		responses, errs = discordMessage.SendMessage()
+	}
+	for _, err := range errs {
+		if err != nil {
+			logger.error.Println(err)
+			os.Exit(5)
+		}
+	}
+	var fail bool
+	for _, response := range responses {
+		logger.debug.Println(response)
+		logger.debug.Println(response.Request)
+		url := response.Request.URL.String()
+		if response.StatusCode != 204 {
+			logger.error.Printf("Failed to send message to %s!", url)
+			fail = true
+		} else {
+			logger.info.Printf("Successfully sent meggase to %s", url)
+		}
+	}
+	if fail {
+		os.Exit(5)
 	}
 }
