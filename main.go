50d58992faafb6e36684e8042b953d4531556e22
Oula Kuuva <oula.kuuva@iki.fi>
2019-07-12 11:17:20 +0300

    Fix config selection in main

--- main.go
+++ main.go
@@ -2,9 +2,13 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/jinzhu/copier"
+	"github.com/okuuva/civ2disgord/civ2disgord"
 )
 
 const (
@@ -21,13 +25,22 @@ func main() {
 	}
 	logger := newLogger(debugPipe, os.Stdout, os.Stderr)
 
-	var config *config
-	var err error
+	var config civ2disgord.DiscordConfig
 	if cmdline.configFilePath != "" {
 		var f io.Reader
+		var err error
 		f, err = os.Open(cmdline.configFilePath)
 		logger.checkFatal(err, "Could not open config file", 1)
-		config, err = parseConfig(&f)
-		logger.checkFatal(err, "Could not parse config file", 2)
+		config, err = civ2disgord.ParseConfig(&f)
+		logger.checkFatal(err, "Could not parse config file", 1)
+	} else if cmdline.useDefaults {
+		err := copier.Copy(&config, &civ2disgord.DefaultDiscordConfig)
+		logger.checkFatal(err, "Failed to access default config", 2)
+	} else {
+		logger.checkFatal(errors.New("no config provided"), "No config provided", 3)
+	}
+	if len(cmdline.messages) == 0 {
+		logger.checkFatal(errors.New("no message given"), "No message given", 3)
+	}
 	}
 }
