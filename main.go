2f20cf5055e6d9f0b5c85e3021b702d7ab3994e5
Oula Kuuva <oula.kuuva@iki.fi>
2019-07-13 13:42:09 +0300

    Move checkers to separate file

--- main.go
+++ main.go
@@ -17,31 +17,6 @@ const (
 	usage = "Halp"
 )
 
-func checkResponses(responses []*http.Response, logger *logger) bool {
-	success := true
-	for _, response := range responses {
-		logger.debug.Println(response)
-		logger.debug.Println(response.Request)
-		url := response.Request.URL.String()
-		if response.StatusCode != 204 {
-			logger.error.Printf("Failed to send message to %s!", url)
-			success = false
-		} else {
-			logger.info.Printf("Successfully sent meggase to %s", url)
-		}
-	}
-	return success
-}
-
-func checkErrors(errs []error) error {
-	for _, err := range errs {
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func main() {
 	cmdline := parseSettings()
 	var debugPipe io.Writer
